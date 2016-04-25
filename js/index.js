468379b2603082e748e73daf6b93575f1c2ec321
WarnerHooh <Warner.Hooh@gmail.com>
2016-04-25 18:07:08 +0800

    update index.js

--- js/index.js
+++ js/index.js
@@ -1,19 +1,16 @@
 import React from 'react-native'
 import { Provider } from 'react-redux'
-import configureStore from './store/configureStore'
+import store from './store'
 import App from './App'
 
 class Root extends Component{
   constructor(props) {
     super(props)
-    this.state = {
-      store: configureStore(() => this.setState())
-    }
   }
 
   render() {
     return (
-        <Provider store={this.state.store}>
+        <Provider store={store}>
           <App />
         </Provider>
       )
